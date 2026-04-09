6e191d02fda3c5b0ea968c0da815f5fd4b00ee1b
vosien <202865739+vosien@users.noreply.github.com>
2026-04-08 22:33:04 -0500

    feat: change cell background color & opacity on hover

--- js/index.js
+++ js/index.js
@@ -3,6 +3,18 @@ const canvas = document.querySelector('#canvas');
 for (let i = 0; i < 256; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.addEventListener('mouseenter', (e) => {
+        let bgColor = cell.style.backgroundColor;
+        if (bgColor === '') {
+            cell.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255) + 1} ${Math.floor(Math.random() * 255) + 1} ${Math.floor(Math.random() * 255) + 1} / 0.1)`;
+        } else if (bgColor !== '') {
+            let bgArr = bgColor.split(',');
+            const opacity = bgArr[3] !== undefined ? parseFloat(bgArr[3]) : 1;
+            if (opacity < 1) {
+                cell.style.backgroundColor = `${bgArr.slice(0, 3).join(',')}, ${Math.min(1, opacity + 0.1)})`;
+            }
+        }
+    })
+
     canvas.append(cell);
 }
-
