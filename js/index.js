16b3950d9f6145dad8e015bd04049d9b3b42be2d
vosien <202865739+vosien@users.noreply.github.com>
2026-04-09 16:36:53 -0500

    refactor: clarify rgb & remove redundancy in opacity increment logic

--- js/index.js
+++ js/index.js
@@ -21,16 +21,18 @@ function setGrid() {
         cell.addEventListener('mouseenter', () => {
             let bgColor = cell.style.backgroundColor;
             if (bgColor === '') {
-                cell.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255) + 1} ${Math.floor(Math.random() * 255) + 1} ${Math.floor(Math.random() * 255) + 1} / 0.1)`;
+                const r = Math.floor(Math.random() * 256);
+                const g = Math.floor(Math.random() * 256);
+                const b = Math.floor(Math.random() * 256);
+                cell.style.backgroundColor = `rgba(${r}, ${g}, ${b}, 0.1)`;
             } else if (bgColor !== '' && bgColor[3] === 'a') {
                 let bgArr = bgColor.split(',');
                 const opacity = bgArr[3] !== undefined ? parseFloat(bgArr[3]) : 1;
                 if (opacity < 1) {
-                    cell.style.backgroundColor = `${bgArr.slice(0, 3).join(',')}, ${Math.min(1, opacity + 0.1)})`;
+                    cell.style.backgroundColor = `${bgArr.slice(0, 3).join(',')}, ${(opacity + 0.1).toFixed(1)}`;
                 }
             }
         })
-
         canvas.append(cell);
     }
 }
